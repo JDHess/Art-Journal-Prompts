ba1ee7bd97a71b26f8b6ff12656579dc38453bd3
Julie Hess <jul>
2024-03-03 12:46:26 -0800

    Initial Commit

---
+++ Art Journal Prompts/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Art Journal Prompts
+//
+//  Created by Julie Hess on 3/3/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Art Journal Prompts/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  Art Journal Prompts
+//
+//  Created by Julie Hess on 3/3/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *GenerateButton;
+
+@property (weak, nonatomic) IBOutlet UITextView *Generation;
+
+
+
+@end
+
